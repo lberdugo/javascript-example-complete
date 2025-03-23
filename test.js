97772b3cee198de2b01a7fd443dbd896dee75555
Liat Berdugo <lberdugo@users.noreply.github.com>
2025-03-23 09:58:09 -0700

    send to server

---
+++ test.js
@@ -0,0 +1,9 @@
+
+
+function changeText(){
+    var myh1 = document.getElementById('test');
+
+    myh1.innerHTML = "you clicked!";
+    myh1.style.color = "white";
+    myh1.style.fontSize = "100px";
+}
